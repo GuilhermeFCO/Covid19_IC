ae5435704917ac4b1e4e1f812b36370eacfb6485
GuilhermeFCO <guilherme.f.castro@ufv.br>
2021-02-17 11:38:58 -0300

    Corr

--- graphs.R
+++ graphs.R
@@ -1,18 +1,20 @@
 library(tidyverse)
-library(sta)
+library(stats)
 #------------------------------------------------------------------------------#
 source("data/merge.R")
 #------------------------------------------------------------------------------#
 # Teste Mapa ------------------------------------------------------------------#
 
+
 teste <- covid_IDH_Maps %>% 
     dplyr::filter(place_type == "city", state == "MG")
 
 ggplot(data = teste) +
-    geom_sf(data = teste, aes(fill=IDHM, geometry = geom), color= NA, size=.15) +
+    geom_sf(data = teste, aes(fill=corr, geometry = geom), color= NA, size=.15) +
     labs(title="IDH dos Municipíos de MG") +
-    scale_fill_distiller(palette = "Greens", 
-                         limits=c(min(teste$IDHM),max(teste2$IDHM)),
+    scale_fill_distiller(palette = "Reds",
+                         direction = 1,
+                         limits=c(min(teste$corr),max(teste$corr)),
                          name="IDH")  +
     theme_void()
 
@@ -24,7 +26,8 @@ teste2 <- covid_IDH_Maps %>%
 ggplot(data = teste2) +
     geom_sf(data = teste2, aes(fill=IDHM, geometry = geom), color= NA, size=.15) +
     labs(title="IDH dos Estados do Brasil") +
-    scale_fill_distiller(palette = "Blues", 
+    scale_fill_distiller(palette = "Blues",
+                         direction = 1,
                          limits=c(min(teste2$IDHM),max(teste2$IDHM)),
                          name="IDH") +
     theme_void()
@@ -67,4 +70,33 @@ fitState3 <- stats::lm(confirmed ~ estimated_population_2019 +
                            IDHM, data = state_covid)
 summary(fitState3)
 
+# corr ------------------------------------------------------------------------#
+
+states_IDH_maps <- covid_IDH_Maps %>% 
+    dplyr::filter(place_type == "state")
+states_IDH_maps$corr <- 1
+
+for (i in 1:nrow(states_IDH_maps)) {
+    df <- NULL
+    df <- covidIDH_last %>% 
+        dplyr::filter(place_type == "city", state == states_IDH_maps$state[i])
+    correlation <- NULL
+    correlation <- stats::cor(df$death_rate, df$IDHM)
+    states_IDH_maps$corr[[i]] <- correlation
+}
+rm("df", "correlation", "i")
+
+
+ggplot(data = states_IDH_maps) +
+    geom_sf(data = states_IDH_maps, aes(fill=corr, geometry = geom), 
+            color= NA, size=.15) +
+    labs(title="Correlação dos Estados do Brasil") +
+    scale_fill_distiller(palette = "Reds",
+                         direction = 1,
+                         limits=c(min(states_IDH_maps$corr),
+                                  max(states_IDH_maps$corr)),
+                         name="Corr") +
+    theme_void()
+
 #------------------------------------------------------------------------------#
+
