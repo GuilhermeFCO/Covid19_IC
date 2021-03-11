dd9f0bd7cc19fcf5da9de5470ab88abada77a895
GuilhermeFCO <guilherme.f.castro@ufv.br>
2021-03-11 19:11:21 -0300

    1

--- server.R
+++ server.R
@@ -1,3 +1,37 @@
+library(shiny)
 server <- function(input, output, sesion) {
     
+    output$mapBrasil <- plotly::renderPlotly({
+        if (input$divBrasil == "Regiões") {
+            dataBrasil <- regionStates
+        } else if (input$divBrasil == "Estados") {
+            dataBrasil <- states
+        } else if (input$divBrasil == "Cidades") {
+            dataBrasil <- cities
+        } else if (input$divBrasil == "Mesorregiões") {
+            dataBrasil <- meso
+        } else if (input$divBrasil == "Microrregiões") {
+            dataBrasil <- micro
+        }
+        
+        dataBrasil %>% 
+            ggplot() +
+            geom_sf(aes(fill=confirmed, geometry = geom),
+                    color= NA, size=.15) +
+            labs(title="Confirmados") +
+            scale_fill_distiller(palette = "Oranges",
+                                 direction = 1,
+                                 limits=c(min(dataBrasil$confirmed),
+                                          max(dataBrasil$confirmed)),
+                                 name="Número de Casos Confirmados") +
+            theme_void()
+    })
+    
+    # output$graBrasil0 <- plotly::plotlyOutput(
+    #     
+    # )
+    # 
+    # output$graBrasil1 <- plotly::plotlyOutput(
+    #     
+    # )
 }
--- ui.R
+++ ui.R
@@ -1,22 +1,73 @@
-ui <- fluidPage(includeCSS("styles/style.css"), navbarPage("Covid-19 no Brasil",
+library(shiny)
+library(plotly)
+
+ui <- fluidPage(includeCSS("styles/style.css"), 
+navbarPage("Covid-19 no Brasil",
     tabPanel("Descrição"),
     tabPanel("Covid",
         sidebarLayout(
             tags$div(style = "width: 1200px;",
-                sidebarPanel(
-                    width = 4
+            sidebarPanel(
+                conditionalPanel(condition = "input.tabselectedCovid == 0",
+                    selectInput(
+                        inputId = "divBrasil",
+                        label = "Divisão por:",
+                        choices = c("Regiões",
+                                    "Estados",
+                                    "Cidades",
+                                    "Mesorregiões",
+                                    "Microrregiões"),
+                        selected = "Regiões",
+                        selectize = TRUE
+                    )                    
+                ),
+                conditionalPanel(condition = "input.tabselectedCovid == 1",
+                                 h1("TESTE1")
+                ),
+                conditionalPanel(condition = "input.tabselectedCovid == 2",
+                                 h1("TESTE2")
+                ),
+                conditionalPanel(condition = "input.tabselectedCovid == 3",
+                                 h1("TESTE3")
+                ),
+                conditionalPanel(condition = "input.tabselectedCovid == 4",
+                                 h1("TESTE4")
+                ),
+                conditionalPanel(condition = "input.tabselectedCovid == 5",
+                                 h1("TESTE5")
                 )
-            ),
+            )),
             mainPanel(
                 fluidPage(
                     fluidRow(
                         column(10,
-                            tabsetPanel(
-                                tabPanel("Regiões"),
-                                tabPanel("Estados"),
-                                tabPanel("Cidades"),
-                                tabPanel("Mesorregiões"),
-                                tabPanel("Microrregiões")
+                            tabsetPanel(id = "tabselectedCovid",
+                                tabPanel("Brasil", value = 0,
+                                     fluidRow(
+                                        column(10,
+                                            plotly::plotlyOutput(
+                                                outputId = "mapBrasil"
+                                            )
+                                        )    
+                                     ),
+                                     fluidRow(
+                                         column(5,
+                                             plotly::plotlyOutput(
+                                                 outputId = "graBrasil0"
+                                             )
+                                         ),
+                                         column(5,
+                                             plotly::plotlyOutput(
+                                                 outputId = "graBrasil1"
+                                             )
+                                         )
+                                     )
+                                ),
+                                tabPanel("Regiões", value = 1),
+                                tabPanel("Estados", value = 2),
+                                tabPanel("Cidades", value = 3),
+                                tabPanel("Mesorregiões", value = 4),
+                                tabPanel("Microrregiões", value = 5)
                             )
                         ),
                         column(2,
